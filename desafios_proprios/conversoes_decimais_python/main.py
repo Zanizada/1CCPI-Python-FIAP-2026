6a5954ffd654baed73edc9afcdb7cb0dfed28fc4
Zanizada <rafaelgizzi@gmail.com>
2026-03-19 10:40:38 -0300

    Atualização das linhas em que tinha a função "conversor()" para "conversor_dec()".

--- desafios_proprios/conversoes_decimais_python/main.py
+++ desafios_proprios/conversoes_decimais_python/main.py
@@ -1,4 +1,4 @@
-from functions import conversor
+from functions import conversor_dec
 
 opcao = int(input("Quer converter decimal para outra base, ou de uma base especifica para decimal? (1/2)"))
 
@@ -6,5 +6,5 @@ if opcao == 1:
     numero = int(input("Digite o numero que vai ser convertido: "))
     base = int(input("Digite a base do resultado final: "))
 
-    resultado = conversor(numero, base)
+    resultado = conversor_dec(numero, base)
     print(f"O numero {numero} na base {base} eh: {resultado}")
