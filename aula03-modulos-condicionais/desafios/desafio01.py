eb0376cbef809d18dba397eb7b2a2bad190cace0
Zanizada <rafaelgizzi@gmail.com>
2026-03-30 11:45:23 -0300

    Desafio 1, um código que determina se o usuário não pode, pode ou deve votar nas eleições com base na sua idade.

--- aula03-modulos-condicionais/desafios/desafio01.py
+++ aula03-modulos-condicionais/desafios/desafio01.py
@@ -1,6 +1,8 @@
 idade = int(input("Digite sua idade: "))
 
-if idade < 16:
+if idade < 1:
+    print("Você não existe.")
+elif idade < 16:
     print("Não pode votar!")
 elif 16 <= idade < 18 or idade > 70:
     print("Pode votar!")
