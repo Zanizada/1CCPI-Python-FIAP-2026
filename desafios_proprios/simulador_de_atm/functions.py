7d78a3ecafd6f9e9f950008a5193568bfbbe3071
Rafael Zani Gizzi <rafaelgizzi@gmail.com>
2026-04-09 20:25:30 -0300

    Criação de função para digitação de senha camuflada.

--- desafios_proprios/simulador_de_atm/functions.py
+++ desafios_proprios/simulador_de_atm/functions.py
@@ -1,3 +1,5 @@
+import msvcrt
+
 def moneyformat(valor):
     return f"R$ {valor:.2f}"
 
@@ -9,3 +11,23 @@ def notasformat(valor): # 180
         lista_notas.append(f'{qntd_notas} nota(s) de R$ {nota}.00')
         valor %= nota
     return lista_notas
+
+def input_senha(prompt="Digite sua senha: "):
+    print(prompt, end="", flush=True)
+    senha = ""
+
+    while True:
+        char = msvcrt.getch()
+
+        if char in {b'\r', b'\n'}:  # Enter
+            print()
+            break
+        elif char == b'\x08':  # Backspace
+            if len(senha) > 0:
+                senha = senha[:-1]
+                print("\b \b", end="", flush=True)
+        else:
+            senha += char.decode("utf-8")
+            print("*", end="", flush=True)
+
+    return senha
